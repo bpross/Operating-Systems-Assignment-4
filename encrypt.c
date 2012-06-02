3c0f57cd7f3a7379939d827b112a589b8561d786
Benjamin Ross <benr22@gmail.com>
2012-06-01 18:43:52 -0700

    Still trying

--- encrypt.c
+++ encrypt.c
@@ -1,8 +1,20 @@
-#include <stdlib.h>
+#include <minix/callnr.h>
+#include <signal.h>
+#include <sys/svrctl.h>
+#include <sys/resource.h>
+#include <sys/utsname.h>
+#include <minix/com.h>
+#include <minix/config.h>
+#include <minix/sysinfo.h>
+#include <minix/type.h>
+#include <minix/vm.h>
+#include <machine/archtypes.h>
 #include <lib.h>
-
+#include <assert.h>
+#include <stdio.h>
 void do_setkey()
 {
+    
     int k0 = m.in.m1_i1;
     int k1 = m.in.m1_i2;
 
