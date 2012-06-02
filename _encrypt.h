c340e75694be253e54f4f0bc931fbf61a333aa58
Benjamin Ross <benr22@gmail.com>
2012-06-01 19:22:58 -0700

    ASDFASDFASDF

--- _encrypt.h
+++ _encrypt.h
@@ -6,6 +6,7 @@ PUBLIC void setkey(unsigned int k0, unsigned int k1)
     message m;
     m.m1_i1 = k0;
     m.m1_i2 = k1;
-
-    return(_syscall(MFS_PROC_NR, SETKEY, &m));
+    int retvalue;
+    retvalue = _syscall(MFS_PROC_NR, SETKEY, &m);
+    printf("Return value: %d\n",retvalue);
 }
