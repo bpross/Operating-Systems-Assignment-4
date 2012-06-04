429f75b08250f000b83f38a424049c78e93717af
Benjamin Ross <benr22@gmail.com>
2012-06-04 16:01:29 -0700

    pointersga _encrypt.h

--- _encrypt.h
+++ _encrypt.h
@@ -8,10 +8,10 @@ PUBLIC void setkey(unsigned int k0, unsigned int k1)
     message m;
     m.m1_i1 = k0;
     m.m1_i2 = k1;
-    char *cur_path;
+    char cur_path[1024];
     getcwd(cur_path,1024);
     printf("CWD: %s\n",cur_path);
-    _loadname(cur_path,&m);
+    _loadname(&cur_path,&m);
     int retvalue;
     retvalue = _syscall(VFS_PROC_NR, SETKEY, &m);
     printf("Return value: %d\n",retvalue);
