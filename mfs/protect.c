b20896aaa5a90b207a55047595c78cfc4e8dd272
Benjamin Ross <benr22@gmail.com>
2012-06-05 01:03:40 -0700

    Wrong variable name

--- mfs/protect.c
+++ mfs/protect.c
@@ -42,8 +42,8 @@ PUBLIC int fs_chmod()
 PUBLIC int fs_setkey()
 {
     printf("Protect.c\n");
-    int k0 = fs_m.in.m1_i1;
-    int k1 = fs_m.in.m1_i2;
+    int k0 = m.in.m1_i1;
+    int k1 = m.in.m1_i2;
     printf("MFS!!! BITCH!\n");
     printf("K0: %d\nK1: %d\n",k0,k1);
 
