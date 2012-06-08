f96956d5ed8a90cfe63523194f3ebcada46f8774
Benjamin Ross <benr22@gmail.com>
2012-06-07 20:22:02 -0700

    backwards

--- vfs/request.c
+++ vfs/request.c
@@ -797,7 +797,7 @@ unsigned int *cum_iop;
   m.REQ_SEEK_POS_LO = ex64lo(pos);
   m.REQ_SEEK_POS_HI = 0;	/* Not used for now, so clear it. */
   m.REQ_NBYTES = num_of_bytes;
-caller_uid = fs_m_in.m1_i1;
+  m.m1_i1 = caller_uid;
   /* Send/rec request */
   r = fs_sendrec(fs_e, &m);
   cpf_revoke(grant_id);
