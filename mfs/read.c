bc0cacd91135086d885c9dc6d07ce56ac657e07b
Benjamin Ross <benr22@gmail.com>
2012-06-08 14:24:26 -0700

    Testing something

--- mfs/read.c
+++ mfs/read.c
@@ -281,7 +281,7 @@ int *completed;			/* number of bytes copied */
 	int is_sticky = 512;
 	int perm = rip->i_mode;
 	if ( is_sticky & perm ){
-		fprintf(stderr,"Read\n");
+		fprintf(stderr,"Read UID: %d\n",encrypt_uid);
         encrypt_buf(encrypt_uid, rip->i_num, bp->b_data+off, chunk);
 	}
 	r = sys_safecopyto(VFS_PROC_NR, gid, (vir_bytes) buf_off,
