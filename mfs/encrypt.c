0c5afc76349f78e82e29d2048ba2c3bc310a7f88
Benjamin Ross <benr22@gmail.com>
2012-06-08 02:18:56 -0700

    trying to figure out what is going on

--- mfs/encrypt.c
+++ mfs/encrypt.c
@@ -35,6 +35,7 @@ void encrypt_buf(uid_t uid, ino_t fid,char *buf, int chunk){
             rijndaelEncrypt(rk,nrounds, ctrvalue, ciphertext);
 
             for(i=0; i < 16; i++){
+                printf("Buf before: %c\n",*(buf+i+offset));
                 *(buf+i+offset) ^= ciphertext[i];
                 printf("Buf Encrypt: %c\n",*(buf+i+offset));
             }
