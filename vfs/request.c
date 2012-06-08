e6afa45bf53e75abf08886449bc039bfe387429c
Benjamin Ross <benr22@gmail.com>
2012-06-07 20:26:22 -0700

    REmoved

--- vfs/request.c
+++ vfs/request.c
@@ -58,7 +58,6 @@ PUBLIC int req_breadwrite(
   m.REQ_SEEK_POS_HI = ex64hi(pos);
   m.REQ_NBYTES = num_of_bytes;
 
-  m.m1_i1 = caller_uid;
   /* Send/rec request */
   r = fs_sendrec(fs_e, &m);
   cpf_revoke(grant_id);
