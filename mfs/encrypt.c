f6d6d069967d46c956d5194cf0641564c51418a2
Benjamin Ross <benr22@gmail.com>
2012-06-08 02:08:18 -0700

    Trying to see what is happening

--- mfs/encrypt.c
+++ mfs/encrypt.c
@@ -36,7 +36,9 @@ void encrypt_buf(uid_t uid, ino_t fid,char *buf, int chunk){
 
             for(i=0; i < 16; i++){
                 *(buf+i+offset) ^= ciphertext[i];
+                printf("Buf Encrypt: %c",*(buf+i+offset));
             }
+            printf("\n");
             offset += 16;
         }
     }
@@ -47,6 +49,7 @@ void encrypt_buf(uid_t uid, ino_t fid,char *buf, int chunk){
 
         for(i=0; i < chunk; i++){
             buf[i] ^= ciphertext[i];
+            printf("Buf Encrypt: %c",*(buf+i+offset));
         }
     }
 }
