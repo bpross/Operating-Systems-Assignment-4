a38cf9d4bcc9c42747fafd20569ba5c831c17b08
Matthew Menzel Musselman <mmusselm@unix3.ic.ucsc.edu>
2012-06-09 03:03:10 -0700

    Updated key_table.h.

--- mfs/key_table.h
+++ mfs/key_table.h
@@ -18,90 +18,41 @@ typedef unsigned char u8;
 //typedef struct key_entry *key_entry_ref;
 typedef u32 **key_table_ref;
 
-/*
- * add_to_table: kt, userid, key
- * @param kt: Key table to add key to
- * @param userid: userid to add to the table
- * @param key: key to map to the user
- * This method is used to add a user/key to the table
- * IT DOES NOT CHECK IF THE USER HAS ANOTHER KEY
- * (this will be added if we have time, but the spec does
- * not call for this)
- */
-int add_to_table(key_table_ref kt, uid_t userid, unsigned char* key); //tested
 
-/*
- * remove_from_table: kt, userid
- * @param kt: Key table to remove key from
- * @param userid: userid to remove from the table
- * This method searches the table for an entry associated with userid and
- * deletes it.
- * If no entry exists for userid, returns -1
- */
-int remove_from_table(key_table_ref kt, uid_t userid);
+/* new_table */
+key_table_ref init_table(key_table_ref kt);
 
-/*
- * print_table: kt
- * @param kt: Table to print
- * This prints the table to stdout in the following fashion:
- * UID: USERID  KEY: USERKEY
- */
-void print_table(key_table_ref kt); //tested
-
-/*
- * table_full: kt
- * @param kt: Table to check if full
- * This checks to see if a key can be added to the table
- * returns 0 if entries < MAX_KEY
- */
-int table_full (key_table_ref kt); //tested
-
-/*
- * set_uid: e, uid
- * @param e: entry in the table to set the userid
- * @param uid: the userid to use
- * This sets the userid for the appropriate entry
- */
-key_entry_ref set_uid (key_entry_ref e, uid_t uid); //tested
-/*
- * get_uid: e
- * @param e: entry in the table to set the userid
- * This gets the userid for the appropriate entry
- */
-uid_t get_uid(key_entry_ref e); //tested
-/*
- * set_key: e, key
- * @param e: entry in the table to se the key
- * @param key: the key to use
- * This sets the key for the appropriate entry
- */
-key_entry_ref set_key (key_entry_ref e,unsigned char* key); //tested
-
-/*
- * get_key: e, key
- * @param e: entry in the table to se the key
- * This gets the key for the appropriate entry
- */
-unsigned char * get_key (key_entry_ref e); //tested
-/*
- * new_table: void
- * This creates a new key table. Sets entries to 0
- */
-key_table_ref init_table(void); //tested
-
-// /*
- // * free_table: kt
- // * @param kt: table to be freed
- // * This frees the table
- // */
+// /* free_table */
 // void free_table(key_table_ref kt);
 
-/*
- * get_key_by_uid: kt, id
- * @param kt: key table to look at
- * @param id: ID to get key for
- * Returns -1 if no key found
- * This grabs the key for the id passed
- */
+/* set_uid */
+int set_uid (key_table_ref kt, int u_index, uid_t uid);
+
+/* get_uid */
+uid_t get_uid(key_table_ref kt, int u_index);
+
+/* set_key */
+int set_key (key_table_ref kt, int u_index, u32 k0, u32 k1);
+
+/* get_key */
+u8* get_key(key_table_ref kt, u8* key, int u_index);
+
+/* is_empty */
+int is_empty (key_table_ref kt, int i);
+
+/* table_full */
+int table_full (key_table_ref kt);
+
+/* print_table */
+void print_table (key_table_ref kt);
+
+int has_key(key_table_ref kt, uid_t userid);
+
+/* remove_from_table */
+int remove_from_table(key_table_ref kt, uid_t userid);
+            
+/* add_to_table */
+int add_to_table(key_table_ref kt, uid_t userid, u32 k0, u32 k1);
+
 unsigned char* get_key_by_uid(key_table_ref kt, uid_t id);
 #endif
