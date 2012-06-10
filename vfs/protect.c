4971ea6127fbc04e41b16eec0cd313c6175a7d26
Benjamin Ross <benr22@gmail.com>
2012-06-10 15:20:11 -0700

    Typos

--- vfs/protect.c
+++ vfs/protect.c
@@ -123,9 +123,9 @@ PUBLIC int do_chmod()
     m.fd = m_in.fd;
     m.buffer = new_buf;
     m.nbytes = vp->v_size;
-    q = read_wite(READING,flp,new_buf,vp->v_size,who_e);
+    q = read_write(READING,flp,new_buf,vp->v_size,who_e);
     printf("Q: %d\n",q);
-    q = read_write(WRITE,flp,new_buf,vp->v_size,whoe);
+    q = read_write(WRITE,flp,new_buf,vp->v_size,who_e);
     printf("Q: %d\n",q);
   }
   put_vnode(vp);
