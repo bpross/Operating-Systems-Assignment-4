327001cfa5fac094540904c8736f1b7d70d1bbc9
Benjamin Ross <benr22@gmail.com>
2012-06-09 22:56:08 -0700

    This is my first attempt at the system command

---
+++ setkey/setkey.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <_encrypt.h>
+
+int main(int argc, char** argv)
+{
+    if (argc < 3){
+        fprintf(stderr,"Usage: %s <key1> <key2>\n",argv[0]);
+        return 1;
+    }
+
+    int k0;
+    int k1;
+
+    /* Get variables from the commandline */
+    k0 = strtol(argv[1],NULL,0);
+    k1 = strtol(argv[2],NULL,0);
+
+    setkey(k0,k1);
+}
