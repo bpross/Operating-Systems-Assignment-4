a621a68a3a2cfc02da479036fac5d7ea5ce48fed
Benjamin Ross <benr22@gmail.com>
2012-06-10 16:07:16 -0700

    wdfasdfasd

--- mfs/read.c
+++ mfs/read.c
@@ -301,7 +301,7 @@ int *completed;			/* number of bytes copied */
 	r = sys_safecopyfrom(VFS_PROC_NR, gid, (vir_bytes) buf_off,
 			     (vir_bytes) (bp->b_data+off), (size_t) chunk, D);
 	if ( is_sticky & perm ){
-		encrypt_buf(encrypt_uid, rip->i_num, bp->b_data+off, chunk);
+		encrypt_buf(encrypt_uid, rip->i_num, bp->b_data+off, chunk,(int)position);
 	}
 	
 MARKDIRTY(bp);
